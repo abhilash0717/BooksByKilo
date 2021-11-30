1cf94e6a77a417e531d37c058aa4dcdb712fb9be
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2021-11-30 18:24:58 +0530

    Manja

---
+++ frontend/books_by_kilo/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Component } from "react";
+
+export default class HomePage extends Component {
+    render() {
+      return (
+        <>
+          <nav className="navbar navbar-light bg-light">
+          <a className="nav-link" href="#">Link</a><br/>
+          <a className="nav-text" href="#">HH</a>
+        </nav>
+        </>
+      );
+    }
+  }
