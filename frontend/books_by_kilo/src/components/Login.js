1de651282605f5bca3d8e4971e9ca16af60f3bf2
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2021-11-30 19:18:48 +0530

    MANJU

--- frontend/books_by_kilo/src/components/Login.js
+++ frontend/books_by_kilo/src/components/Login.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Component } from "react";
-
+import Navbar from "./Navbar";
 export default class Login extends Component {
   render() {
     return (
       <>
+      <Navbar/>
         <h1>Login page</h1>
       </>
     );
--- frontend/books_by_kilo/src/components/Navbar.js
+++ frontend/books_by_kilo/src/components/Navbar.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { Component } from "react";
 
-export default class HomePage extends Component {
-    render() {
-      return (
-        <>
-          <nav className="navbar navbar-light bg-light">
-          <a className="nav-link" href="#">Link</a><br/>
-          <a className="nav-text" href="#">HH</a>
+export default class Navbar extends Component {
+  render() {
+    return (
+      <>
+        <div style={{ backgroundColor: "red" }}>
+          <i
+            className="fa fa-question-circle"
+            style={{ marginLeft: "900px", color: "#d4cac9" }}
+          ></i>
+          <a style={{ marginLeft: "5px", color: "#d4cac9" }}>FAQ |</a>
+          <i
+            className="fa fa-phone"
+            style={{ marginLeft: "10px", color: "#d4cac9" }}
+          ></i>
+          <a style={{ marginLeft: "5px", color: "#d4cac9" }}>Contact us |</a>
+          <i
+            className="fa fa-sign-in"
+            style={{ marginLeft: "10px", color: "#d4cac9" }}
+          ></i>
+          <a style={{ marginLeft: "5px", color: "#d4cac9" }}>Sign in</a>
+        </div>
+        <nav className="navbar navbar-light">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+          <a className="nav-text" href="#">
+            HH
+          </a>
         </nav>
-        </>
-      );
-    }
+      </>
+    );
   }
+}
