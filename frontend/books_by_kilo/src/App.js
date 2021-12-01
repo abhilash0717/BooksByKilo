7e2a7100541dbd3b32092114c5ecc146374a59cf
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2021-12-01 18:24:44 +0530

    FE latest

--- frontend/books_by_kilo/src/App.js
+++ frontend/books_by_kilo/src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 import HomePage from "./components/HomePage";
 import Navbar from "./components/Navbar"
 import Login from "./components/Login";
-
+import Faq from "./components/Faq";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
@@ -13,10 +14,14 @@ function App() {
           <Link to="/home" />
           <Link to="/login" />
           <Link to="/navbar" />
+          <Link to="/faq"/>
+          <Link to="/contactUs"/>
           <Routes>
             <Route path="/home" exact element={<HomePage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/navbar" element={<Navbar />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/contactUs" element={<ContactUs />} />
 
           </Routes>
         </>
