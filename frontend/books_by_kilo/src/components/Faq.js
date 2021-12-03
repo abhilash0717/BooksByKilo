a188be6a5327889c71274e3c62ab7426485a2293
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2021-12-03 13:12:17 +0530

    Faq

--- frontend/books_by_kilo/src/components/Faq.js
+++ frontend/books_by_kilo/src/components/Faq.js
@@ -3,6 +3,12 @@ import react from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 export default class Faq extends Component{
+    state={toggle : false}
+
+    plusClick = () => {
+        console.log("clicked");
+        this.setState({toggle:!this.state.toggle})
+      };    
 render(){
     return(
         <>
@@ -10,7 +16,14 @@ render(){
         <h1>Frequently Asked Questions</h1>
         <div className="container">
         <div style={{border:"2px solid black"}}>
-           <p style={{paddingTop:"10px", marginRight:"900px"}}> How good are the Books?</p>
+           <p style={{paddingTop:"10px"}}> How good are the Books?<i 
+             className="fa fa-plus"
+            style={{ marginLeft: "850px" }}
+           onClick={()=>this.plusClick()}></i></p> 
+         {this.state.toggle ? 
+         <div> 
+             <p>answer</p> 
+         </div>:null}
         </div>
         </div>
         <Footer/>
