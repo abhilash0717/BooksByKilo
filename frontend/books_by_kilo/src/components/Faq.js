282ad45dc25ff38dfc321a9be98c446ecc5745ca
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2021-12-03 11:02:55 +0530

    FAQ change

--- frontend/books_by_kilo/src/components/Faq.js
+++ frontend/books_by_kilo/src/components/Faq.js
@@ -1,10 +1,19 @@
 import { Component } from "react"
 import react from "react"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 export default class Faq extends Component{
 render(){
     return(
         <>
-        <h1>FAQ page</h1>
+        <Navbar/>
+        <h1>Frequently Asked Questions</h1>
+        <div className="container">
+        <div style={{border:"2px solid black"}}>
+           <p style={{paddingTop:"10px", marginRight:"900px"}}> How good are the Books?</p>
+        </div>
+        </div>
+        <Footer/>
         </>
     )
 }
