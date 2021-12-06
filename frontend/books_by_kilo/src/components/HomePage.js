53519c6c97470b351386fa3dd4e85f12fef65a1a
abhilash0717 <40905548+abhilash0717@users.noreply.github.com>
2021-12-06 17:23:28 +0530

    Modified HomePage cards

--- frontend/books_by_kilo/src/components/HomePage.js
+++ frontend/books_by_kilo/src/components/HomePage.js
@@ -1,30 +1,42 @@
 import React from "react";
 import { Component } from "react";
 import Navbar from "./Navbar";
-import "../App.css"
+import "../App.css";
 export default class HomePage extends Component {
   render() {
     return (
       <>
-      <Navbar/>
-        <h1>Home Page</h1>
-        <div className="container">
-    <div className="card">
-      <div className="card-body">
-        <img src="https://www.booksbykilo.in/media/staticimages/HomepageNewBooks.png"/>
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn">Go somewhere</a>
-    </div>
-    <div className="card">
-      <div className="card-body">
-        <img src="https://www.booksbykilo.in/media/staticimages/HomepageNewBooks.png"/>
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn">Go somewhere</a>
-    </div></div>
-  </div>
-</div></>
-  );
-}
+        <Navbar />
+        <div className="row">
+          <div className="col-md-3" style={{ marginLeft: "30px" }}>
+            <div className="card">
+              <div className="card-body">
+                <img src="https://www.booksbykilo.in/media/staticimages/HomepageNewBooks.png" />
+                <h4 style={{ marginRight: "140px" }}>New books</h4>
+                <div className="row">
+                  <div className="col-md-8">
+                    <h6>Discount up to 80%</h6>
+                    <p>Hot new best-selling releases</p>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="button2">
+                      <button className="btn button2">Explore</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card">
+              <div className="card-body">
+                <img src="https://www.booksbykilo.in/media/staticimages/HomepageNewBooks.png" />
+                <h5 className="card-title">Card title</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
