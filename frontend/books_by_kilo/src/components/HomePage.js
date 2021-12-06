cd55ed6af185fd071b10631610f7abb2abbe6e98
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2021-12-06 17:26:06 +0530

    no need

--- frontend/books_by_kilo/src/components/HomePage.js
+++ frontend/books_by_kilo/src/components/HomePage.js
@@ -9,12 +9,12 @@ export default class HomePage extends Component {
       <Navbar/>
         <h1>Home Page</h1>
         <div className="container">
-    <div className="card">
+    <div className="card col-md-3">
       <div className="card-body">
         <img src="https://www.booksbykilo.in/media/staticimages/HomepageNewBooks.png"/>
         <h5 className="card-title">Card title</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn">Go somewhere</a>
+        <a href="#" className="btn">Explore</a>
     </div>
     <div className="card">
       <div className="card-body">
