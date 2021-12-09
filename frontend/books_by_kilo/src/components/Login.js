45e73daf39b6367932f0ba5016aac1d5b28cff34
abhilash0717 <40905548+abhilash0717@users.noreply.github.com>
2021-12-09 23:22:09 +0530

    Made changes in login and register components

--- frontend/books_by_kilo/src/components/Login.js
+++ frontend/books_by_kilo/src/components/Login.js
@@ -12,14 +12,34 @@ export default class Login extends Component {
       <>
         <Navbar />
         <div>
-  <h1>Login to your account</h1><br/>
-  <input className="form-control input" type="text" placeholder="Phone Number" style={{width:"600px", marginLeft:"350px"}}/><br/>
-  <button className="btn button2">Send OTP</button><br/><br/>
-  </div>
-  <div>
-    <label>New customer register here?</label>
-    <d><span style={{color:"red"}} onClick={()=>this.Register()} >&nbsp;Register Here</span></d>
-  </div>
+          <h1>Login to your account</h1>
+          <br />
+          <form
+            className="col-4 offset-4"
+            style={{ border: "1px solid #f0ece1" }}
+          >
+            <input
+              className="form-control input"
+              type="text"
+              placeholder="Phone Number"
+              style={{ marginTop: "40px" }}
+            />
+            <br />
+            <button className="btn button2">Send OTP</button>
+            <br />
+            <br />
+
+            <div>
+              <label>New customer register here?</label>
+              <d>
+                <span style={{ color: "red" }} onClick={() => this.Register()}>
+                  &nbsp;Register Here
+                </span>
+              </d>
+            </div>
+            <br />
+          </form>
+        </div>
         <Footer />
       </>
     );
--- frontend/books_by_kilo/src/components/Register.js
+++ frontend/books_by_kilo/src/components/Register.js
@@ -1,32 +1,61 @@
 import { Component } from "react";
-import "../App.css"
+import "../App.css";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-export default class Register extends Component{
-    signin = () => {
-        console.log("clicked");
-        window.location = "/login";
-      };
-    render(){
-        return(
-            <>
-            <Navbar/>
-            <h1>Create new account</h1><br/>
-    
-            <div>
-                <input className="form-control input"type="text" placeholder="Name" style={{width:"500px", marginLeft:"350px"}}/><br/>
-                <input className="form-control input"type="text" placeholder="Phone Number" style={{width:"500px", marginLeft:"350px"}}/><br/>
-                <input className="form-control input"type="text" placeholder="Email" style={{width:"500px", marginLeft:"350px"}}/><br/>
-                </div>
-                  <div>
-               <button className="btn button2">Send OTP</button></div><br/><br/>
-            
-            <div>
-    <label>Already register</label>
-    <d><span style={{color:"red"}} onClick={()=>this.signin()} >&nbsp;Login here</span></d>
-  </div>
-  <Footer/>
-            </>
-        )
-    }
+export default class Register extends Component {
+  signin = () => {
+    console.log("clicked");
+    window.location = "/login";
+  };
+  render() {
+    return (
+      <>
+        <Navbar />
+        <h1>Create new account</h1>
+        <br />
+
+        <form
+          className="col-4 offset-4"
+          style={{ border: "1px solid #f0ece1" }}
+        >
+          <div>
+            <input
+              className="form-control input"
+              type="text"
+              placeholder="Name"
+              style={{ marginTop: "40px" }}
+            />
+            <br />
+            <input
+              className="form-control input"
+              type="text"
+              placeholder="Phone Number"
+            />
+            <br />
+            <input
+              className="form-control input"
+              type="text"
+              placeholder="Email"
+            />
+            <br />
+          </div>
+          <div>
+            <button className="btn button2">Send OTP</button>
+          </div>
+          <br />
+
+          <div>
+            <label>Already registered?</label>
+            <d>
+              <span style={{ color: "red" }} onClick={() => this.signin()}>
+                &nbsp; Login here
+              </span>
+            </d>
+          </div>
+        </form>
+
+        <Footer />
+      </>
+    );
+  }
 }
