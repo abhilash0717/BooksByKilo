7fb7668dd89e7726a2ee44af83173ad058d56e7e
Manjunathan-16 <manjunathan.16.1998@gmail.com>
2021-12-09 18:00:11 +0530

    New books page have been changed

--- frontend/books_by_kilo/src/components/NewBooks.js
+++ frontend/books_by_kilo/src/components/NewBooks.js
@@ -1,9 +1,74 @@
 import { Component } from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 export default class NewBooks extends Component {
+    newBooks = () => {
+        console.log("clicked");
+        window.location = "/newBooks";
+      };
+      premiumCollection = () => {
+        console.log("clicked");
+        window.location = "/premiumCollection";
+      };
+      classicCollection = () => {
+        console.log("clicked");
+        window.location = "/classicCollection";
+      };
+      standardCollection = () => {
+        console.log("clicked");
+        window.location = "/standardCollection";
+      };
 render(){
     return(
         <>
-        <h1>New Books</h1>
+        <Navbar/>
+        <div className="row">
+            <div className="col-md-2" style={{marginLeft:"20px"}}>
+<h6>COLLECTION</h6>
+<a>
+              {" "}
+<span
+                className="nav-link"
+                style={{ color: "black"}}
+                onClick={() => this.newBooks()}
+              >
+                New Books
+              </span>
+            </a>
+            <a>
+              {" "}
+              <span
+                className="nav-link"
+                style={{ color: "black"}}
+                onClick={() => this.premiumCollection()}
+              >
+                Premium Collection
+              </span>
+            </a>
+            <a>
+              <span
+                className="nav-link"
+                style={{ color: "black"}}
+                onClick={() => this.classicCollection()}
+              >
+                Classic Collection
+              </span>
+            </a>
+            <a>
+              <span
+                className="nav-link"
+                style={{ color: "black"}}
+                onClick={() => this.standardCollection()}
+              >
+                Standard Collection
+              </span>
+            </a>
+            </div>
+            <div className="col-md-8">
+<h1>Books</h1>
+</div>
+        </div>
+        <Footer/>
         </>
     )
 }
