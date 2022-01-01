6ea00ebf5e1f3885659e0a8f07d742d2c6dba349
abhilash0717 <40905548+abhilash0717@users.noreply.github.com>
2022-01-01 14:22:21 +0530

    final commit

--- frontend/books_by_kilo/src/components/Register.js
+++ frontend/books_by_kilo/src/components/Register.js
@@ -1,11 +1,41 @@
+import axios from "axios";
 import { Component } from "react";
 import "../App.css";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+
+const url = "http://localhost:4001/BooksByKilo/Register"
 export default class Register extends Component {
+  state = {
+    Username: "",
+    Password: "",
+    successMessage  : ""
+  };
   signin = () => {
     console.log("clicked");
     window.location = "/login";
+  };
+
+  Register = () => {
+    axios.post(url.concat("/" + this.state.Username + "/" + this.state.Password), {})
+    .then((response)=>{
+      this.setState({successMessage : response.data})
+      console.log(this.state.successMessage);
+    })
+    .catch();
+  }
+
+  inputChangeUsername = (e) => {
+    e.preventDefault();
+    this.setState({ Username: e.target.value });
+    console.log(this.state.Username);
+    window.localStorage.setItem("Username", e.target.value);
+  };
+  inputChangePassword = (e) => {
+    e.preventDefault();
+    this.setState({ Password: e.target.value });
+    console.log(this.state.Password);
+    window.localStorage.setItem("Password", e.target.value);
   };
   render() {
     return (
@@ -20,13 +50,19 @@ export default class Register extends Component {
         >
           <div>
             <input
+              name="Username"
+              value={this.state.Username}
               className="form-control input"
+              onChange={(e) => this.inputChangeUsername(e)}
               type="text"
               placeholder="Username"
               style={{ marginTop: "40px" }}
             />
             <br />
             <input
+              name="Password"
+              value={this.state.Password}
+              onChange={(e) => this.inputChangePassword(e)}
               className="form-control input"
               type="password"
               placeholder="Password"
@@ -35,7 +71,9 @@ export default class Register extends Component {
             <br />
           </div>
           <div>
-            <button className="btn button2">Register</button>
+            <button className="btn button2" onclick={() => this.Register()}>
+              Register
+            </button>
           </div>
           <br />
 
